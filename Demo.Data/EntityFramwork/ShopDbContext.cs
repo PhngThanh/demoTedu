08e241c89c28fe0261b2b68ea3eec21d926268c0
Thanh Vũ <phngthanh2310@gmail.com>
2020-12-04 21:51:46 +0700

    design DB

---
+++ Demo.Data/EntityFramwork/ShopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Demo.Data.configurations;
+using Demo.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Data.EntityFramwork
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfig());
+
+            //base.OnModelCreating(modelBuilder);
+        }
+        //khai báo entity
+        //bao nhiêu entity khai báo bấy nhiêu
+        public DbSet<Product> Products { get; set; }
+
+        //ccaaus hình quan hệ
+        //confi entity
+        //1. Attribute confi
+        //2. Fluent API - k động class mà confi ở ngoài
+    }
+}
